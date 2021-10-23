759e48b7c70c0cd12657ed7b9f7a05a4836da986
av-riel <89614254+av-riel@users.noreply.github.com>
2021-10-22 22:25:13 -0400

    update 10.25

--- 02-Save/Enemy/Scanning.gd
+++ 02-Save/Enemy/Scanning.gd
@@ -4,14 +4,12 @@ onready var SM = get_parent()
 onready var enemy = get_node("../..")
 onready var ray = enemy.get_node("RayCast2D")
 
+var turret_pos = 0
+
 func _ready():
 	yield(enemy, "ready")
 
 func physics_process(_delta):
-	if enemy.increasing:
-		enemy.increase_turret()
-	else:
-		enemy.decrease_turret()
 	enemy.set_turret_angle(enemy.turret_pos)
 	enemy.set_ray_angle(enemy.turret_pos)
 	ray.cast_to = Vector2(-10000,0)
@@ -19,3 +17,12 @@ func physics_process(_delta):
 	var c = ray.get_collider()
 	if c and c.name == "Player":
 		SM.set_state("Searching")
+
+
+#	if enemy.increasing:
+#		enemy.increase_turret()
+#	else:
+#		enemy.decrease_turret()
+#
+#
+#
